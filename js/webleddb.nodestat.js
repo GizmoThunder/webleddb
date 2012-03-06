61c535829e1da0b493b5d42528cef4bc486b8074
vamsikrishna <vamsikrishna@beg.com>
2012-03-06 12:38:00 +0100

    modifying node stat to use predefined styles. styles available in node.html

--- js/webleddb.nodestat.js
+++ js/webleddb.nodestat.js
@@ -1,4 +1,4 @@
-(function($) {
+(function($){
     $.widget("webleddb.nodestat", {
 		options: {
             'nodeid': 'Node 007',
@@ -15,86 +15,74 @@
             //label for the node.
             var nodeid = this.options.nodeid;
             var title = $( "<div></div>" ).
-                        css({
-                            'position':'relative',
-                            'width':'30px',
-                            'left':'-32px',
-                            'top': '-1px',
-                            'height':'30px',
-                            'text-align':'center',
-                            'font-weight': 'bold',
-                            }).append( "Node " + nodeid.slice( nodeid.indexOf('e') + 1) ). //assuming node id is of "nodexxx" form.
-                            addClass( 'ui-state-active' );
-                            
+                            append( "<p>" + nodeid + "</p>" ).
+                            addClass( 'ui-state-active nodestat-title' );
+            if( this.options.cpu < 0 ){
+                title.removeClass( 'ui-state-active' ).addClass( 'ui-state-error' );
+            }
            //assume we get combined info of all the cpus.
-           var cpus = $( "<div></div>" ).
-               css({
-                   'position':'relative',
-                   'top': '-32px',
-                   'width': '99px'
-               }).append( '<b>Cpu:</b>' );
+            var cpuinfo = $( "<div></div>" ).
+                addClass( 'nodestat-cpus' ).
+                append( '<b>Cpu:</b>' );
 
-           var cpu_label = $( "<div></div>" ).
-               css({
-                   'position':'relative',
-                   'top':'-12px',
-                   'width':'100%',
-                   'text-align':'center',
-                   'font-weight':'bold'
-               });
+            var cpu_label = $( "<div></div>" ).addClass( 'nodestat-progressbar-label' );
 
-           var cpu = $( "<div></div>" ).
-               css({
-               'height': '12px', 
-               'margin': '2px'
-               }).
-               progressbar({
+            var cpu = $( "<div></div>" ).
+                addClass( 'nodestat-progressbar' ).
+                progressbar({
                    value:this.options.cpu
                }).append( cpu_label );
            cpu_label.html( cpu.progressbar( "option", "value" ) + "%" );
-           cpus.append( cpu );
+           cpuinfo.append( cpu );
+
+           var meminfo = $( "<div></div>" ).
+                addClass( 'nodestat-cpus' ).
+                append( '<b>Mem:</b>' );
+
+            var mem_label = $( "<div></div>" ).addClass( 'nodestat-progressbar-label' );
+
+            var mem = $( "<div></div>" ).
+                addClass( 'nodestat-progressbar' ).
+                progressbar({
+                   value:this.options.mem
+               }).append( mem_label );
+           mem_label.html( mem.progressbar( "option", "value" ) + "%" );
+           meminfo.append( mem );
+           cpuinfo.append( meminfo );
+ 
  
             //disks on this node.
             var disks = $( "<div></div>" ).
-                        css({
-                            'position':'relative',
-                            'top': '-32px',
-                            'width' : '99px'
-                        }).append( '<b>Disks:</b>' ); 
+                    addClass( 'nodestat-disks' ).
+                    append( '<b>Disk:</b>' ); 
             for( var i = 0; i < this.options.disks.length; i++ ){
                 var disk_info = this.options.disks[i];
                 var label = $( "<div></div>" ).
-                    css({
-                            'position':'relative',
-                            'top':'-12px',
-                            'width':'100%',
-                            'text-align':'center',
-                            'font-weight':'bold'
-                            });
+                            addClass( 'nodestat-progressbar-label' );
 
                 var disk = $( "<div></div>" ).
-                    css( {
-                            'height': '12px',
-                            'margin': '2px'
-                            }).
-                    progressbar({
-                        value: disk_info[0] 
-                    }).append( label );
-                    label.html( disk.progressbar( "option", "value" ) + "%");
+                    addClass( 'nodestat-progressbar' );
+                    if( disk_info[0] < 0 ){
+                       disk.addClass( 'ui-state-error ui-corner-all' );     
+                       label.html( "NA" ).css({
+                        "text-align": "center",
+                        "font-weight": "bold"
+                        }).removeClass('nodestat-progressbar-label');
+                    }else{
+                        disk.progressbar({
+                                value: disk_info[0] 
+                            });
+                        label.html( disk.progressbar( "option", "value" ) + "%");
+                    }
+                    disk.append( label );
                     disks.append( disk );
             }
 
             this.element.html(""); //clear all stuff in this element.
             //add the new info to the element.
             this.element.
-                addClass( 'ui-widget-content ui-state-highlight' ).
-                    css({
-                        margin: '20px',
-                        padding: '5px',
-                        float: 'left',
-                        width: "100px",
-                        height: "100px",
-                    }).append( title, cpus, disks );
+                addClass( 'nodestat  ui-state-highlight' ).
+                append( title, cpuinfo, disks );
 		},
 		
         _setOptions: function(){
